fc4153b7510aa56e4d04e14a2e959f1731ea82af
vitaly1234567890 <Troyanvitaly123@yandex.ru>
2023-12-01 12:50:49 +0300

    7 commit

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,30 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            return [...state.sort((a, b) => {
+                if(action.payload === 'up'){
+                    if(a.name < b.name) {
+                        return -1
+                    }
+                } else if (action.payload === 'down') {
+                    if(a.name > b.name) {
+                        return -1
+                    }
+                } return 0
+            })]
 
-            return state // need to fix
+            // [...state.sort((a, b) =>
+            //     action.payload === 'up' ? a.name.localeCompare(b.name)
+            //         : action.payload === 'down'
+            //         ? b.name.localeCompare(a.name)
+            //         : b.name.localeCompare(a.name)
+            // )]
         }
         case 'check': {
-
-            return state // need to fix
+            return [...state.filter((u) => u.age>=action.payload)] // need to fix
         }
         default:
             return state
