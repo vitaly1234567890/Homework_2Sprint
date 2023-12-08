e15bb75ba11ef30610568778d6ff65137a628000
vitaly1234567890 <Troyanvitaly123@yandex.ru>
2023-12-08 11:04:42 +0300

    9 commit

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -37,6 +37,7 @@ function Clock() {
         setShow(false)
     }
 
+
     const stringTime = date.toLocaleTimeString('en-US', {hour12: false}) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = date.toLocaleDateString('en-US', { year: 'numeric', month: 'numeric', day: 'numeric' }).replace(/\//g, '.') || <br/>; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
