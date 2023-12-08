2f256f83598dfd9c1dea2af4e783b266ab742d06
vitaly1234567890 <Troyanvitaly123@yandex.ru>
2023-12-08 11:54:17 +0300

    9 commit

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -37,9 +37,12 @@ function Clock() {
         setShow(false)
     }
 
+    const dateWithZero = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+    const monthWithZero = date.getMonth() < 10 ? '0' + date.getMonth() : date.getMonth()
 
     const stringTime = date.toLocaleTimeString('en-US', {hour12: false}) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleDateString('en-US', { year: 'numeric', month: 'numeric', day: 'numeric' }).replace(/\//g, '.') || <br/>; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate =
+        (`${dateWithZero.toString()}.${(+monthWithZero + 1).toString()}.${date.getFullYear().toString()}`)|| <br/>; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = date.toLocaleDateString('en-US', {weekday: "long"}) || <br/> // пишут студенты
